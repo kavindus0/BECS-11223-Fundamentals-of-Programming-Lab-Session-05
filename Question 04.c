f3f0c4cff1d5df6a79cf0a4bcfd7d5bbbc5168f5
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-12 00:55:26 +0530

    Question 04

---
+++ Question 04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+/*
+ Enter character: A
+ ASCII value of A is: 65
+ */
+
+printf("Enter character: ");
+char y;
+scanf("%c",&y);
+printf("ASCII value of %c is: %d\n",y,y);
+
+
+return 0;
+}
