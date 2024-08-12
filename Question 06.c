2d5cb81c5a90a3cf7b03cec50890cce845c4fd5b
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-08-12 11:51:58 +0530

    Update Question 06.c

--- Question 06.c
+++ Question 06.c
@@ -2,35 +2,35 @@
 
 int main(){
 /*
- Quesstion 05.c
- 5. Write a C program to determine an employee weekly salary based on the following information.
- • Read the employee regular hourly pay rate from user.
- • Read the employee hours worked from the user.
- • Calculate the total weekly pay for the employee. (No overtime pay for this example.)
- • Then, display the results in the following example:
- Test data and expected output:
- Enter hourly pay rate:200
- Enter hours worked: 50
- ********* Weekly Salary *********
- Hourly Rate:
- 200.00
- Hours Worked:
- 50.00
- Weekly Salary:
- 10000.00
+
+Write a C program to implement a simple receipt generator. This shop sells only two products and calculate
+the total bill amount based on the quantity purchased by the user and display receipt according the following
+guidelines.
+• Unit price for the Laptop is 120,000.00 and Mobile Phone is 55,000.00.
+• Read number of laptops purchased by the customer.
+• Read number of phones purchased by the customer.
+• Then, calculate the total amount for the receipt and display it in the following format:
+Enter Number of Laptop Purchased:2
+Enter Number of Phones Purchased: 10
+********* Welcome UoK Digital Store ********************
+Description No of Units Unit Price Sub Total
+Laptop 2 120000.00 240000.00
+Phone 10 55000.00 550000.00
+Total Amount 790000.00
+----------------------------------------------------------------------------------------------------
  */
 
-    printf("Enter hourly pay rate:");
-    float hrRate;
-    scanf("%f",&hrRate);
-    printf("Enter hours worked:");
-    float hrs;
-    scanf("%f",&hrs);
-    
-printf("********* Weekly Salary *********\n");
-    printf("Hourly Rate:\n%.2f",hrRate);
-    printf("\nHours Worked:\n%.2f",hrs);printf("\nWeekly Salary:\n%.2f\n",hrs*hrRate);
-    
+float unitLaptop = 120000;
+float unitMobile = 55000;
+printf("Enter Number of Laptop Purchased:");
+int laptopNo,mobileNo;
+scanf("%d",&laptopNo);
+printf("Enter Number of Phones Purchased:");
+scanf("%d",&mobileNo);
+printf("********* Welcome UoK Digital Store ********************\n");
+printf("Description\tNo of Units\tUnit Price\tSub Total");
+printf("\nLaptop\t\t%d\t%.2f\t%.2f\n",laptopNo,unitLaptop,laptopNo*unitLaptop);
+printf("Phone\t\t%d\t%.2f\t%.2f",mobileNo,unitMobile,mobileNo*unitMobile);
 
 
 return 0;
